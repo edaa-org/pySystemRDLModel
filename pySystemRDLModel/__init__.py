bb9d554c43e86f789bd43155cbb17106e55acd33
Patrick Lehmann <Paebbels@gmail.com>
2023-08-01 23:25:38 +0200

    Bumped version to v0.2.0.

--- pySystemRDLModel/__init__.py
+++ pySystemRDLModel/__init__.py
@@ -44,7 +44,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2023-2023, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.1.0"
+__version__ =   "0.2.0"
 
 
 @export
