e43bbc2cb4953aebf17ae90efaff0b5da91a3f57
Patrick Lehmann <Paebbels@gmail.com>
2026-07-04 22:44:44 +0200

    Updated Sphinx configuration for LuaLaTeX and Unicode font support.

--- run.ps1
+++ run.ps1
@@ -33,7 +33,7 @@ Param(
 )
 
 $PackageName = "pySystemRDLModel"
-$PackageVersion = "0.3.9"
+$PackageVersion = "0.3.10"
 
 # set default values
 $EnableDebug =        [bool]$PSCmdlet.MyInvocation.BoundParameters["Debug"]
